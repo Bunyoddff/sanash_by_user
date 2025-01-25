962e8c92dd87421a17196cb9129c7c20e46b68dd
Bunyoddff <bkhusanov444@gmail.com>
2025-01-25 14:27:01 +0500

    without CallbackQuereyHandler

---
+++ main_sanash.py
@@ -0,0 +1,48 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackContext,Filters,CallbackQueryHandler
+import os
+from telegram import Update,ReplyKeyboardMarkup,InlineKeyboardButton,InlineKeyboardMarkup,Bot
+
+sana_like={'like':0, 'dislike':0}
+
+# Get the bot token from environment variables
+TOKEN = os.getenv('TOKEN')
+
+# def info(update: Update, context):
+#     inline_button = InlineKeyboardButton(text="Siz yuborgan rasm",callback_data=1)
+#     inline_keyboard = InlineKeyboardMarkup([[inline_button,inline_button]])
+    
+#     update.message.reply_text('nima gap',reply_markup=inline_keyboard)
+
+def bronnarsa_qaytaradi(update: Update, context: CallbackContext):
+    ismi = update.message.from_user.full_name
+    update.message.reply_text(ismi)
+def rasm_qaytar(update:Update, context:CallbackContext):
+
+    rasm_manzili=update.message.photo[-1].file_id
+    tugma1 = InlineKeyboardButton(text=f"Do you like 👍{sana_like['like']} ",callback_data=1)
+    tugma2 = InlineKeyboardButton(text=f"Do you dislike 👎 {sana_like['dislike']}",callback_data=1)
+    inline_keyboard = InlineKeyboardMarkup([[tugma1,tugma2]])
+    update.message.reply_photo(photo=rasm_manzili,reply_markup=inline_keyboard)
+
+
+# def likedislike(update: Update, context: CallbackContext):
+#     global like_sanagich,dislike_sanagich
+#     if update.message.text=='👎':
+#         dislike_sanagich+=1
+#         update.message.reply_text(f'Dislekelar soni {dislike_sanagich} ga teng,Likelar soni {like_sanagich} ga teng')
+#     if update.message.text=='👍':
+#         like_sanagich+=1
+#         update.message.reply_text(f'Dislekelar soni {dislike_sanagich} ga teng,Likelar soni {like_sanagich} ga teng')
+updater = Updater(token=TOKEN, use_context=True)
+dispatcher = updater.dispatcher
+
+# Add handlers
+dispatcher.add_handler(CommandHandler('start', bronnarsa_qaytaradi))
+#dispatcher.add_handler(CommandHandler('info',info))
+dispatcher.add_handler(MessageHandler(Filters.photo,rasm_qaytar))
+
+# Start the bot
+updater.start_polling()
+
+# Run the bot until you stop it
+updater.idle()
